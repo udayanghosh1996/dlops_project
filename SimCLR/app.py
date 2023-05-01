a1bf47ca6506a2c24e93a1281e4bb13e40450ebf
Udayan Ghosh <udayanghosh1996@gmail.com>
2023-05-01 23:57:29 +0530

    Initial commit

--- SimCLR/app.py
+++ SimCLR/app.py
@@ -3,67 +3,33 @@ import multiprocessing
 
 import os
 
-from flask import Flask, jsonify, request, render_template
 from predict_image import *
 import numpy as np
-import cv2
 
-must_reload_page = False
-
-
-def start_flask():
-    if not hasattr(st, 'already_started_server'):
-        st.already_started_server = True
-        must_reload_page = True
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main():
+    st.title("Image Prediction")
 
-    app = Flask(__name__)
+    menu = ["Home", "Image Prediction", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
 
-    @app.route('/')
-    def home():
-        return render_template('home.html')
+    if choice == "Image prediction":
+        st.subheader("Home")
+        image_file = st.file_uploader("Upload Image", type=['png', 'jpeg', 'jpg'])
+        if image_file is not None:
+            file_details = {"Filename": image_file.name, "FileType": image_file.type, "FileSize": image_file.size}
+            st.write(file_details)
 
-    @app.route('/prediction', methods=['GET', 'POST'])
-    def prediction():
-        '''if request.get_json() is not None:
-            json_ = request.json
-            pred = image_prediction(json_['image1'])
-            if pred is not None:
-                return jsonify(pred)'''
-
-        if request.data is not None:
-            npar = np.fromstring(request.data, np.uint8)
-            img = cv2.imdecode(npar, cv2.IMREAD_COLOR)
-            # path = os.path.join(os.path.join(os.getcwd(), '../webpages'), 'img.jpg')
-            # imge = cv2.imread(path)
-            image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            image = load_image(image_file)
             pred = image_prediction(image)
-            return jsonify(pred)
-
-    @app.route('/dummy', methods=['GET', 'POST'])
-    def dummy():
-        if request.data is not None:
-            prob = np.random.random_sample(size=10)
-            clas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-            dic = dict(zip(clas, prob))
-            return dic
-
-    app.run(host='https://udayanghosh1996-dlops-project--simclrapp-9aqgnl.streamlit.app', debug=True)
-
-
-def reload_page():
-    if must_reload_page:
-        must_reload_page = False
-        st.experimental_rerun()
+            st.write(pred)
+    else:
+        st.subheader("About")
+        st.info("DL-Ops Project")
+        st.text("Udayan Ghosh, Souvik Pal, Sourav Banerjee")
 
 
 if __name__ == '__main__':
-    flask_process = multiprocessing.Process(target=start_flask)
-    reload_process = multiprocessing.Process(target=reload_page)
-    flask_process.start()
-    reload_process.start()
-
-x = st.slider('Pick a number')
-st.write('You picked:', x)
+    main()
